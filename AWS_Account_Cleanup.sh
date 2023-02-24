c746a70ae17403a99b4618c3561a0c89c4e881ba
tcooksey1972 <32277716+tcooksey1972@users.noreply.github.com>
2023-02-24 17:50:06 -0500

    Create AWS_Account_Cleanup.sh

---
+++ AWS_Account_Cleanup.sh
@@ -0,0 +1,5 @@
+It is not recommended to clean up an entire AWS account and reset all resources to their virgin state without 
+first understanding the implications and verifying the impact on your environment. This script is for educational 
+purposes only, and it is strongly advised that you understand each step before running the script in your own AWS account.
+
+AWS_Account_Cleanup.sh Script can be used as a starting point for resetting an AWS account:
